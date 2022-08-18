5b87af2451111eba869e11a2088afd531a6a1904
Vitalii Dokalenko <vitaliidokalenko@gmail.com>
2022-08-18 17:37:02 +0300

    initial commit

---
+++ src/main/java/ua/com/secondked/SecondkedApplication.java
@@ -0,0 +1,13 @@
+package ua.com.secondked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondkedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondkedApplication.class, args);
+	}
+
+}
